7af00d254f33798455822af12ef93534a1460487
dmsch <dschukin@mail.ru>
2017-01-22 20:47:08 +0200

    spa-jade: navbar and carousel

--- assets/js/modules/orangeMainModule.js
+++ assets/js/modules/orangeMainModule.js
@@ -1,26 +1,10 @@
 "use strict";
 
-/*(function () {
+(function () {
   const appOrange = angular.module('orangeMain', []);
 
 
-
-  appOrange.controller('navController', ['$scope', '$location',
-    function ($scope, $location) {
-      $scope.isActiveMenuItem = function (destination) {
-        return destination === $location.path();
-      };
-    }]);
-  
-  appOrange.config(function ($locationProvider) {
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: true,
-      rewriteLinks: false
-    });
-  })
-
-})();*/
+})();
 
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
